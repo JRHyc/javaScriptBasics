368af53e2d4f6ccb73607b2a208d95fb24001c0c
Jack Hyc <Jacek.Hyc@gmail.com>
2018-04-02 11:58:45 -0500

    adding completed assignment

---
+++ jsfund.js
@@ -0,0 +1,49 @@
+// Basic I
+
+// var x = [];
+// console.log(x);
+
+// x.push("coding");
+// x.push("dojo");
+// x.push("rocks");
+// console.log(x);
+
+// x.pop();
+// console.log(x);
+
+// Basic II
+// const y = [];
+// console.log(y);
+
+// y.push(88);
+// y.push(33);
+// console.log(y);
+
+// Basic III
+// var z = [9, 10, 6, 5, -1, 20, 13, 2]
+// console.log(z);
+
+// function print(z){
+// for (var i = 0; i < z.length - 1; i++){
+//     console.log(z[i]);
+// }}
+// print(z);
+
+// Basic IV
+// var names = ['Kadie', 'Joe', 'Fritz', 'Pierre', 'Alphonso'];
+
+// for (var i = 0; i < names.length; i++){
+//     // console.log(names[i].length)
+//     if (names[i].length == 5){
+//         console.log(names[i]);
+//     }
+// }
+
+// Basic V
+// var string = "Hi! My name is Jack!";
+// function yell(string){
+//     string = string.toUpperCase();
+//     console.log(string);
+// }
+
+// yell(string);
